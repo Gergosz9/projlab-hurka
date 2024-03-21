9a1c9ab64c8543a91dafbf7cffe77699757c8081
Szalay Gergő <gergosz9@gmail.com>
2024-03-21 17:55:02 +0100

    Beer class finished

--- src/Java/Beer.java
+++ src/Java/Beer.java
@@ -1,5 +1,35 @@
 package Java;
 
+/**
+ * The Beer class represents a beer item in the game.
+ * It extends the Item class
+ */
 public class Beer extends Item{
+    /**
+     * Constructs a new Beer object with the specified durability.
+     * 
+     * @param durability the durability of the beer
+     */
+    public Beer(int durability){
+        super(durability);
+    }
     
+    /**
+     * Uses the beer item based on the given trigger and source character.
+     * If the trigger is NewRound, the durability of the beer decreases by 1,
+     * the source character's teacher resist attribute is set to true,
+     * and if the durability reaches 0, the source character drops the beer.
+     * 
+     * @param trigger the trigger that activates the use of the beer
+     * @param source the character using the beer
+     */
+    public void use(ItemTrigger trigger, Character source){
+        if(trigger == ItemTrigger.NewRound){
+            durability--;
+            source.setTeacherResist(true);
+            if(durability == 0){
+                source.drop(this, null); //pontosítani kell
+            }
+        }
+    }
 }
