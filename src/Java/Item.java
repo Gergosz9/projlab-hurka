c0e7882362169364517696ea8aff84b145cabe06
Szalay Gergő <gergosz9@gmail.com>
2024-03-21 17:04:26 +0100

    Item class ready

--- src/Java/Item.java
+++ src/Java/Item.java
@@ -1,5 +1,9 @@
 package Java;
 
 public abstract class Item {
-    
+    protected int durability;
+    public Item(int durability){
+        this.durability = durability;
+    }
+    public abstract void use(ItemTrigger trigger, Character source); 
 }
