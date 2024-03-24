0efa4ba9ddad6f925ebcef72dadaebfea3fbe860
Halász István <smackdownmaniac@gmail.com>
2024-03-24 14:48:58 +0100

    Camembert Done

--- src/Java/Camembert.java
+++ src/Java/Camembert.java
@@ -1,5 +1,18 @@
 package Java;
 
 public class Camembert extends Item{
+    public Camembert() {
+    	super(1);
+    }
+    
+    public void use(ItemTrigger trigger, Character source){
+        if(trigger == ItemTrigger.UseActiveItem){
+            durability--;
+            source.getMyLocation().setGassed(true);
+            if(durability == 0){
+                source.drop(this, null); //pontosítani kell
+            }
+        }
+    }
     
 }
