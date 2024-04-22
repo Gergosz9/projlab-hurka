5d0769e9a0040c040f8bc54829202243d4051115
Telek Marcell <telek.marcell22@gmail.com>
2024-04-22 20:26:03 +0200

    TVSZ done

--- src/Java/Items/TVSZ.java
+++ src/Java/Items/TVSZ.java
@@ -1,6 +1,7 @@
 package Java.Items;
 
-import Java.Items.Triggers.AttackTrigger;
+import Java.Items.Triggers.*;
+import Java.Characters.*;
 
 public class TVSZ extends Item {
 
@@ -9,15 +10,14 @@ public class TVSZ extends Item {
     }
 
     public void trigger(AttackTrigger at) {
-        /*
-         * Student student = (Student) source;
-         * if (trigger == ItemTrigger.TeacherAttack) {
-         * durability--;
-         * student.setTeacherResist(true);
-         * if (durability == 0) {
-         * source.dropItem(this, null); // pontos�tani kell
-         * }
-         * }
-         */
+        Student student = (Student) at.getCharacter();
+        if (!student.getTeacherResist()) {
+            student.setTeacherResist(true);
+            this.durability -= 1;
+        }
+
+        if (this.durability <= 0) {
+            student.dropItem(this, null);
+        }
     }
 }
