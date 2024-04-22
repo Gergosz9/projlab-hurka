1b998d9b13eefa95f07bb0139560db8b1ae83aad
Telek Marcell <telek.marcell22@gmail.com>
2024-04-22 19:53:05 +0200

    Beer done

--- src/Java/Items/Beer.java
+++ src/Java/Items/Beer.java
@@ -1,30 +1,39 @@
 package Java.Items;
 
+import java.util.Random;
+
 import Java.Items.Triggers.*;
+import Java.Characters.*;
 import Java.Items.Triggers.RoundTrigger;
 
 public class Beer extends Item {
+    private boolean isInUse = false;
+
     public Beer(int durability, boolean isFake) {
         super(durability, isFake);
     }
 
     public void trigger(RoundTrigger rt) {
-        // RT implement
+        if (isInUse) {
+            Student student = (Student) rt.getCharacter();
+            student.setTeacherResist(true);
 
-        /*
-         * Student student = rt.getCharacter();
-         * if (trigger == ItemTrigger.NewRound) {
-         * durability--;
-         * student.setTeacherResist(true);
-         * if (durability == 0) {
-         * source.dropItem(this, null);
-         * }
-         * }
-         * 
-         */
+            durability -= 1;
+        }
     }
 
     public void trigger(ActionTrigger at) {
-        // AT impelment
+        if (!isInUse) {
+            isInUse = true;
+            Student student = (Student) at.getCharacter();
+            student.setTeacherResist(true);
+
+            durability -= 1;
+        } else {
+            Random random = new Random();
+            int randomidx = random.nextInt(0, at.getCharacter().getInventory().size());
+            Item randomitem = at.getCharacter().getInventory().get(randomidx);
+            at.getCharacter().dropItem(randomitem, at.getCharacter().getMyLocation());
+        }
     }
 }
