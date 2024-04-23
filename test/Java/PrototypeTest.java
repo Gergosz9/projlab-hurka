ee4cfba9e7c2d261a3468d17a415163a8f775d8a
Szalay Gergő <gergosz9@gmail.com>
2024-04-23 20:01:04 +0200

    Added new test class for the prototype tests

---
+++ test/Java/PrototypeTest.java
@@ -0,0 +1,12 @@
+package Java;
+
+import Java.Characters.*;
+import Java.Items.*;
+import Java.Items.Triggers.*;
+import Java.util.*;
+
+public class PrototypeTest {
+    public static void main(String[] args) {
+        System.out.println("Prototype Test");
+    }
+}
