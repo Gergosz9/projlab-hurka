831c27bf034671319a81fe8e4f60d0235f7d7dd2
Telek Marcell <telek.marcell22@gmail.com>
2024-04-24 23:16:02 +0200

    fixed formatting

--- test/Java/PrototypeTest.java
+++ test/Java/PrototypeTest.java
@@ -11,6 +11,7 @@ public class PrototypeTest {
     public static void main(String[] args) {
         System.out.println("Prototype Test");
     }
+
     static void test() {
         String gameName = "test-game-status";
         Labirinth labirinth = new Labirinth();
@@ -26,25 +27,25 @@ public class PrototypeTest {
         labirinth.addRoom(openRoom);
 
         Room room = new Room("name", 10, true, false,
-        List.of(closedRoom),
-        List.of(openRoom));
+                List.of(closedRoom),
+                List.of(openRoom));
         room.addCharacter(teacher);
         room.addCharacter(student);
         labirinth.addRoom(room);
 
         Room room1 = new Room("name1", 10, true, false,
-        List.of(closedRoom),
-        List.of(room));
+                List.of(closedRoom),
+                List.of(room));
         room1.addCharacter(teacher);
         labirinth.addRoom(room1);
 
         Room room2 = new Room("name2", 10, true, false,
-        List.of(room1),
-        List.of(room));
+                List.of(room1),
+                List.of(room));
         room2.addCharacter(student);
         labirinth.addRoom(room2);
 
-        //Items
+        // Items
         AirFreshener airFreshener = new AirFreshener(1, true);
         Beer beer = new Beer(3);
         Camembert camembert = new Camembert();
@@ -73,49 +74,49 @@ public class PrototypeTest {
         checkResult("labyrinth is not null", loadedLabirinth != null);
         checkResult("number of rooms in labyrinth", loadedLabirinth.getRooms().size() == labirinth.getRooms().size());
         for (int i = 0; i < loadedLabirinth.getRooms().size(); i++) {
-checkResult(String.format("loaded room [%d] is correct", i),
-loadedLabirinth.getRooms().get(i).equals(labirinth.getRooms().get(i)));
-}
-checkResult("number of characters in labyrinth", loadedLabirinth.getCharacters().size() == labirinth.getCharacters().size());
-for (int i = 0; i < loadedLabirinth.getCharacters().size(); i++) {
-checkResult(String.format("loaded character [%d] is correct", i),
-loadedLabirinth.getCharacters().get(i).equals(labirinth.getCharacters().get(i)));
-checkResult(String.format("loaded character [%d] contains labyrinth", i),
-loadedLabirinth.getCharacters().get(i).getLabirinth() == loadedLabirinth);
+            checkResult(String.format("loaded room [%d] is correct", i),
+                    loadedLabirinth.getRooms().get(i).equals(labirinth.getRooms().get(i)));
+        }
+        checkResult("number of characters in labyrinth",
+                loadedLabirinth.getCharacters().size() == labirinth.getCharacters().size());
+        for (int i = 0; i < loadedLabirinth.getCharacters().size(); i++) {
+            checkResult(String.format("loaded character [%d] is correct", i),
+                    loadedLabirinth.getCharacters().get(i).equals(labirinth.getCharacters().get(i)));
+            checkResult(String.format("loaded character [%d] contains labyrinth", i),
+                    loadedLabirinth.getCharacters().get(i).getLabirinth() == loadedLabirinth);
 
-}
+        }
 
-checkResult("5th room closed room",
-loadedLabirinth.getRooms().get(4).getClosedRooms().get(0) == loadedLabirinth.getRooms().get(3));
-checkResult("5th room open room",
-loadedLabirinth.getRooms().get(4).getOpenRooms().get(0) == loadedLabirinth.getRooms().get(2));
+        checkResult("5th room closed room",
+                loadedLabirinth.getRooms().get(4).getClosedRooms().get(0) == loadedLabirinth.getRooms().get(3));
+        checkResult("5th room open room",
+                loadedLabirinth.getRooms().get(4).getOpenRooms().get(0) == loadedLabirinth.getRooms().get(2));
 
-checkResult("3rd room 1st character",
-loadedLabirinth.getRooms().get(2).getCharacters().get(0) == loadedLabirinth.getCharacters().get(1));
-checkResult("3rd room 2nd character",
-loadedLabirinth.getRooms().get(2).getCharacters().get(1) == loadedLabirinth.getCharacters().get(0));
+        checkResult("3rd room 1st character",
+                loadedLabirinth.getRooms().get(2).getCharacters().get(0) == loadedLabirinth.getCharacters().get(1));
+        checkResult("3rd room 2nd character",
+                loadedLabirinth.getRooms().get(2).getCharacters().get(1) == loadedLabirinth.getCharacters().get(0));
 
-checkResult("3rd room 1st item",
-loadedLabirinth.getRooms().get(2).getItems().get(0).equals(airFreshener));
-checkResult("3rd room 2nd item",
-loadedLabirinth.getRooms().get(2).getItems().get(1).equals(beer));
+        checkResult("3rd room 1st item",
+                loadedLabirinth.getRooms().get(2).getItems().get(0).equals(airFreshener));
+        checkResult("3rd room 2nd item",
+                loadedLabirinth.getRooms().get(2).getItems().get(1).equals(beer));
 
-checkResult("3rd room 1st transistor",
-loadedLabirinth.getRooms().get(2).getTransistors().get(0).equals(transistor));
-checkResult("3rd room 1st transistor has a labyrinth",
-loadedLabirinth.getRooms().get(2).getTransistors().get(0).getLabirinth() != null);
-checkResult("3rd room 2nd transistor",
-loadedLabirinth.getRooms().get(2).getTransistors().get(1).equals(transistor1));
-checkResult("3rd room 2nd transistor has a labyrinth",
-loadedLabirinth.getRooms().get(2).getTransistors().get(0).getLabirinth() != null);
+        checkResult("3rd room 1st transistor",
+                loadedLabirinth.getRooms().get(2).getTransistors().get(0).equals(transistor));
+        checkResult("3rd room 1st transistor has a labyrinth",
+                loadedLabirinth.getRooms().get(2).getTransistors().get(0).getLabirinth() != null);
+        checkResult("3rd room 2nd transistor",
+                loadedLabirinth.getRooms().get(2).getTransistors().get(1).equals(transistor1));
+        checkResult("3rd room 2nd transistor has a labyrinth",
+                loadedLabirinth.getRooms().get(2).getTransistors().get(0).getLabirinth() != null);
 
+        GameStorageUtil.deleteRepository();
+    }
 
-GameStorageUtil.deleteRepository();
-}
-
-static void checkResult(String criterion, boolean assertedValue){
-String result = assertedValue? "OK!" : "Not OK!";
-System.out.println(String.format("\t- %s: \t%s", criterion, result));
-}
+    static void checkResult(String criterion, boolean assertedValue) {
+        String result = assertedValue ? "OK!" : "Not OK!";
+        System.out.println(String.format("\t- %s: \t%s", criterion, result));
+    }
 
 }
