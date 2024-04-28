f71303fafd9aa3afb02908e5f74346ce7b690fde
Halász István <smackdownmaniac@gmail.com>
2024-04-29 00:23:50 +0200

    Test 10-18s init done

--- test/Java/RoomTest.java
+++ test/Java/RoomTest.java
@@ -1,5 +1,20 @@
 package Java;
 
+import java.util.List;
+
+import Java.Labirinth;
+import Java.Room;
+import Java.Characters.Student;
+import Java.Characters.Teacher;
+import Java.Items.AirFreshener;
+import Java.Items.Beer;
+import Java.Items.Camembert;
+import Java.Items.Mask;
+import Java.Items.Rag;
+import Java.Items.SlideRule;
+import Java.Items.Transistor;
+import Java.util.GameStorageUtil;
+
 public class RoomTest {
 
 }
