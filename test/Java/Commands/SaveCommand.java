7840f41fdba3280bfa013358a1cca368c47086e8
Telek Marcell <telek.marcell22@gmail.com>
2024-05-01 12:59:39 +0200

    Fixed save file

--- test/Java/Commands/SaveCommand.java
+++ test/Java/Commands/SaveCommand.java
@@ -3,25 +3,11 @@ package Java.Commands;
 import Java.*;
 import Java.util.JSONUtil;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class SaveCommand implements Command {
 
     @Override
     public void execute(String[] args, Labirinth l) {
         String fileName = args[1] + "_output";
-
-        Path path = Paths.get(fileName);
-        if (!Files.exists(path)) {
-            try {
-                Files.createFile(path);
-                System.out.println("File created: " + fileName);
-            } catch (Exception e) {
-                System.err.println("Error: " + e.getMessage());
-            }
-        }
 
         try {
             JSONUtil.save(fileName, l);
